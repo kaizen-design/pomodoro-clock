6176960879e62f4219e27d266ba89967b0b2093a
Denis Bondarchuk <denis@kaizen-design.ru>
2019-12-05 04:40:55 +0300

    Updates

---
+++ precache-manifest.83447e25fc1ecb9cfd5a272b95c00ebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99437c2c7282300448b614757205332e",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "40f73061bd9e33d310af",
+    "url": "/javascript-calculator/static/css/main.cb0941a1.chunk.css"
+  },
+  {
+    "revision": "01a5b783188408024238",
+    "url": "/javascript-calculator/static/js/2.b8f0fa92.chunk.js"
+  },
+  {
+    "revision": "40f73061bd9e33d310af",
+    "url": "/javascript-calculator/static/js/main.63a7f1bd.chunk.js"
+  },
+  {
+    "revision": "90e683cc408bf17851a3",
+    "url": "/javascript-calculator/static/js/runtime-main.9133faee.js"
+  }
+]);
