20b4286a1db15fbc8e9d784449360e5960b25266
Denis Bondarchuk <denis@kaizen-design.ru>
2019-12-05 04:35:47 +0300

    Initial commit

---
+++ src/templates/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="container py-3 text-center">
+      <small>
+        Designed & built by <a href="https://github.com/kaizen-design" target="_blank" rel="noopener noreferrer">Denis B.</a>
+      </small>
+    </footer>
+  )
+};
+
+export default Footer;
