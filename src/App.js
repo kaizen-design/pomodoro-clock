b124a88a11cd20365f845bd50556e4b5905e8b7b
denis <denis@kaizen-design.ru>
2020-11-29 17:49:48 +0300

    Toggle implementation added

--- src/App.js
+++ src/App.js
@@ -8,58 +8,135 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      breakLength: 5,
+      sessionLength: 25,
+      currentState: 'Session',
+      active: false,
     };
-    //this.handleButtonClick = this.handleButtonClick.bind(this);
+    //this.handleSettingsChange = this.handleSettingsChange.bind(this);
   }
+
+  handleSettingsChange = (e) => {
+    if (!this.state.active) {
+      const id = e.target.id;
+      switch (id) {
+        case 'break-decrement':
+          if (this.state.breakLength > 1) {
+            this.setState({
+              breakLength: this.state.breakLength - 1
+            });
+          }
+          break;
+        case 'break-increment':
+          if (this.state.breakLength < 60) {
+            this.setState({
+              breakLength: this.state.breakLength + 1
+            });
+          }
+          break;
+        case 'session-decrement':
+          if (this.state.sessionLength > 1) {
+            this.setState({
+              sessionLength: this.state.sessionLength - 1
+            });
+          }
+          break;
+        case 'session-increment':
+          if (this.state.sessionLength < 60) {
+            this.setState({
+              sessionLength: this.state.sessionLength + 1
+            });
+          }
+          break;
+      }
+    }
+  };
+
+  toggleClock = (e) => {
+    const button = e.target;
+    if (this.state.active) {
+      button.textContent = 'Start';
+      button.classList.replace('btn-danger', 'btn-primary');
+      this.setState({
+        active: false
+      });
+    } else {
+      button.textContent = 'Pause';
+      button.classList.replace('btn-primary', 'btn-danger');
+      this.setState({
+        active: true
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      breakLength: 5,
+      sessionLength: 25,
+      currentState: 'Session',
+      active: false
+    });
+  };
 
   render() {
     return (
       <div className="h-100 d-flex flex-column">
         <Navbar brand={projectName} />
         <main role="main" className="App container my-auto py-3">
-          <div className="pomodoro-clock">
-            <div className="card text-center">
-              <div className="card-header">
-                <div className="row">
-                  <div className="col">
-                    <h6 id="break-label" className="d-block text-center mb-2">Break Length</h6>
-                    <div className="input-group">
-                      <div className="input-group-prepend">
-                        <button id="break-decrement" className="btn btn-outline-secondary" type="button">-</button>
-                      </div>
-                      <input id="break-length" type="text" className="form-control text-center" value="5" />
-                      <div className="input-group-append">
-                        <button id="break-increment" className="btn btn-outline-secondary" type="button">+</button>
-                      </div>
+          <div className="pomodoro-clock card text-center">
+            <div className="card-header">
+              <div className="row">
+                <div className="col">
+                  <h6 id="break-label" className="d-block text-center mb-2">Break Length</h6>
+                  <div className="input-group">
+                    <div className="input-group-prepend">
+                      <button id="break-decrement" className="btn btn-outline-secondary" type="button" onClick={this.handleSettingsChange}>-</button>
+                    </div>
+                    <input id="break-length"
+                           type="text"
+                           className="form-control text-center"
+                           value={this.state.breakLength}
+                           onChange={this.handleSettingsChange}
+                           readOnly={!!this.state.active}
+                           min="1"
+                           max="60" />
+                    <div className="input-group-append">
+                      <button id="break-increment" className="btn btn-outline-secondary" type="button" onClick={this.handleSettingsChange}>+</button>
                     </div>
                   </div>
-                  <div className="col">
-                    <h6 id="session-label" className="d-block text-center mb-2">Session Length</h6>
-                    <div className="input-group">
-                      <div className="input-group-prepend">
-                        <button id="session-decrement" className="btn btn-outline-secondary" type="button">-</button>
-                      </div>
-                      <input id="session-length" type="text" className="form-control text-center" value="25" />
-                      <div className="input-group-append">
-                        <button id="session-increment" className="btn btn-outline-secondary" type="button">+</button>
-                      </div>
+                </div>
+                <div className="col">
+                  <h6 id="session-label" className="d-block text-center mb-2">Session Length</h6>
+                  <div className="input-group">
+                    <div className="input-group-prepend">
+                      <button id="session-decrement" className="btn btn-outline-secondary" type="button" onClick={this.handleSettingsChange}>-</button>
+                    </div>
+                    <input id="session-length"
+                           type="text"
+                           className="form-control text-center"
+                           value={this.state.sessionLength}
+                           onChange={this.handleSettingsChange}
+                           readOnly={!!this.state.active}
+                           min="1"
+                           max="60" />
+                    <div className="input-group-append">
+                      <button id="session-increment" className="btn btn-outline-secondary" type="button" onClick={this.handleSettingsChange}>+</button>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="card-body">
-                <h6 id="timer-label" className="mb-0">Session</h6>
-                <h2 id="time-left" className="display-1 font-weight-bold mb-0">25:00</h2>
-              </div>
-              <div className="card-footer text-muted">
-                <div className="row">
-                  <div className="col">
-                    <button id="start_stop" type="button" className="btn btn-block btn-primary">Start</button>
-                  </div>
-                  <div className="col">
-                    <button id="reset" type="button" className="btn btn-block btn-secondary">Reset</button>
-                  </div>
+            </div>
+            <div className="card-body py-5">
+              <h6 id="timer-label" className="mb-0">Session</h6>
+              <h2 id="time-left" className="display-1 font-weight-bold mb-0">25:00</h2>
+            </div>
+            <div className="card-footer text-muted">
+              <div className="row">
+                <div className="col">
+                  <button id="start_stop" type="button" className="btn btn-block btn-primary" onClick={this.toggleClock}>Start</button>
+                </div>
+                <div className="col">
+                  <button id="reset" type="button" className="btn btn-block btn-secondary" onClick={this.reset}>Reset</button>
                 </div>
               </div>
             </div>
